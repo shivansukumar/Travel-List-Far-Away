163ba5f6823ca5c3c29032d0142f12dbb81c6fd1
shivansukumar <shivansugiri1998@gmail.com>
2024-02-11 00:21:00 +0530

    form-handlingEvent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
   { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
@@ -22,10 +22,27 @@ function Logo() {
 }
 
 function Form() {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(e.preventDefault());
+    console.log(e);
+  }
+
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 tour?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>ADD</button>
+    </form>
   );
 }
 
@@ -34,7 +51,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
